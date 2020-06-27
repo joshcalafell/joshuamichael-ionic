67b2088e615d55bf2a1ff1b46e924694dc11e177
Joshua Michael <joshua@muthr.local>
2020-06-27 08:54:22 -0700

    feat(Experience WIP)

--- src/app/pages/experience/experience.page.ts
+++ src/app/pages/experience/experience.page.ts
@@ -9,21 +9,30 @@ import { SeoService } from 'src/app/services/seo/seo.service';
 export class ExperiencePage implements OnInit {
 
   items = [
-    'Planning and development of features for both greenfield and enterprise applications',
-    'Writing extensive developer and user documentation',
-    'Isolating and fixing bugs',
-    'Writing unit tests, integration tests, and E2E tests',
-    'Participating in team code review processes',
-    'Working on internationalization (i18n) to translate apps',
-    'Pair programming and TDD as a way of life',
-    'Working with back-end developers to create APIs',
-    'CI (Continuous Integration) pipelines for delivering new work',
-    'Working with git, branches, and git-flow',
-    'Using RESTful APIs as well as WebSocket technologies',
-    'Participating in scrums, and both Agile and KanBan process',
-    'Management and Team Lead Experience',
-    'Training and educating junior team members'
+    { title: 'Project Architecture', content: 'I have planning and project architecture experience, which to me means working to choose the correct technology, creating code patterns using best practices, and viewing an app as the sum of it\'s components as well as being responsible for the direction and releases of applications.' },
+    { title: 'Feature Work and Unit Testing', content: 'Feature development, on both greenfield and legacy codebases, as well as unit testing and E2E testing for all work, with the responsibility of planning and releasing on time.' },
+    { title: 'Feature Work and Unit Testing', content: 'Feature development, on both greenfield and legacy codebases, as well as unit testing and E2E testing for all work, with the responsibility of planning and releasing on time.' },
+    { title: 'Developer and User Documentation', content: ''},
+    { title: 'Developer and User Documentation', content: ''},
+    { title: 'Developer and User Documentation', content: ''},
+    { title: 'Developer and User Documentation', content: ''},
+    { title: 'Developer and User Documentation', content: ''},
+    
+    // 'Writing extensive developer and user documentation',
+    // 'Isolating and fixing bugs',
+    // 'Writing unit tests, integration tests, and E2E tests',
+    // 'Participating in team code review processes',
+    // 'Working on internationalization (i18n) to translate apps',
+    // 'Pair programming and TDD as a way of life',
+    // 'Working with back-end developers to create APIs',
+    // 'CI (Continuous Integration) pipelines for delivering new work',
+    // 'Working with git, branches, and git-flow',
+    // 'Using RESTful APIs as well as WebSocket technologies',
+    // 'Participating in scrums, and both Agile and KanBan process',
+    // 'Management and Team Lead Experience',
+    // 'Training and educating junior team members'
   ];
+
 
   constructor(private seo: SeoService) {}
 
